2257d5932cc808bbde7987d1856e411b7ca4e16d
Nick Carver <nicholascarver2@outlook.com>
2021-05-27 12:31:35 -0500

    changed register and login to use the correct path

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -24,7 +24,7 @@ export const Register = (props) => {
                 "password": password.current.value
             }
 
-            return fetch("http://127.0.0.1:8000/register", {
+            return fetch("https://nac-level-up-api.herokuapp.com/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
